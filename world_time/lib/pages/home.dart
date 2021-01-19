acf8877ef81099e934847d2f617c8055396fa352
Hakimi Ridzuan <mrhakimi88@gmail.com>
2021-01-19 22:58:23 +0800

    Add files via upload

--- world_time/lib/pages/home.dart
+++ world_time/lib/pages/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -8,13 +7,22 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: <Widget>[
-            FlatButton.icon(onPressed: () {Navigator.pushNamed(context, "/location");
-            }, icon: Icon(Icons.edit_location), label: Text("Edit Location")),
+            FlatButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/location");
+                },
+                icon: Icon(Icons.edit_location),
+                label: Text("Edit Location")),
           ],
         ),
       ),
